223aeac49408021bd442112a7d74fc9d7f2b60e4
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2026-02-23 02:32:21 -0300

    Criando arquivo DTO e conectando com cargos

---
+++ demo/src/main/java/com/example/demo/infrastructure/entitiys/CargoDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.infrastructure.entitiys;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CargoDTO {
+    private String cargo;
+    private Long idEquipe;
+}
