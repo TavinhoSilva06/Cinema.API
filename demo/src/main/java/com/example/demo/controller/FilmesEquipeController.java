441d6fac77ae6a1f1ca99b3021542b560676d26b
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2026-04-02 09:51:02 -0300

    FilmesEquipe no backend resolvido

--- demo/src/main/java/com/example/demo/controller/FilmesEquipeController.java
+++ demo/src/main/java/com/example/demo/controller/FilmesEquipeController.java
@@ -3,16 +3,29 @@ package com.example.demo.controller;
 import com.example.demo.business.FilmesEquipeService;
 import com.example.demo.infrastructure.entitiys.Filme;
 import com.example.demo.infrastructure.entitiys.FilmesEquipe;
+import com.example.demo.infrastructure.entitiys.FilmesEquipeDTO;
+import com.example.demo.infrastructure.repository.EquipeRepository;
+import com.example.demo.infrastructure.repository.FilmeRepository;
+import com.example.demo.infrastructure.repository.FilmesEquipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
 
+
+
 @RestController
 @RequestMapping("/filmesequipe")
-
 public class FilmesEquipeController {
+    @Autowired
+    private EquipeRepository equipeRepository;
+
+    @Autowired
+    private FilmeRepository filmeRepository;
+
+    @Autowired
+    private FilmesEquipeRepository repository;
 
     @Autowired
     private FilmesEquipeService service;
@@ -31,10 +44,19 @@ public class FilmesEquipeController {
     }
 
     @PostMapping
-    public FilmesEquipe criar(@RequestBody FilmesEquipe filmesequipe) {
+    public FilmesEquipe criar(@RequestBody FilmesEquipeDTO dto) {
 
-        return service.salvar(filmesequipe);
+        FilmesEquipe fe = new FilmesEquipe();
 
+        fe.setEquipe(
+                equipeRepository.findById(dto.getIdEquipe()).orElse(null)
+        );
+
+        fe.setFilme(
+                filmeRepository.findById(dto.getIdFilme()).orElse(null)
+        );
+
+        return repository.save(fe);
     }
 
     @PutMapping("/{id}")
---
+++ demo/src/main/java/com/example/demo/infrastructure/entitiys/FilmesEquipeDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.infrastructure.entitiys;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FilmesEquipeDTO {
+    private Long idEquipe;
+    private Long idFilme;
+
+    // getters e setters
+}
